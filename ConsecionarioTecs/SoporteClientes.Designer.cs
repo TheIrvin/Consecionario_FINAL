9c08ff25d024172835dc20642ffba2845eff4ec5
TheIrvin <irvincajas72@gmail.com>
2025-03-01 17:41:13 -0500

    Agregar archivos de proyecto.

---
+++ ConsecionarioTecs/SoporteClientes.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace FormularioDeInicio
+{
+    partial class SoporteClientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblxSoporteClientes = new System.Windows.Forms.Label();
+            this.lblSoporteClientes = new System.Windows.Forms.Label();
+            this.panaleta2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEnviarReporte = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panaleta2.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblxSoporteClientes
+            // 
+            this.lblxSoporteClientes.AutoSize = true;
+            this.lblxSoporteClientes.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblxSoporteClientes.Location = new System.Drawing.Point(771, 0);
+            this.lblxSoporteClientes.Name = "lblxSoporteClientes";
+            this.lblxSoporteClientes.Size = new System.Drawing.Size(27, 26);
+            this.lblxSoporteClientes.TabIndex = 4;
+            this.lblxSoporteClientes.Text = "X";
+            this.lblxSoporteClientes.Click += new System.EventHandler(this.lblxSoporteClientes_Click);
+            // 
+            // lblSoporteClientes
+            // 
+            this.lblSoporteClientes.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblSoporteClientes.AutoSize = true;
+            this.lblSoporteClientes.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoporteClientes.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblSoporteClientes.Location = new System.Drawing.Point(11, 0);
+            this.lblSoporteClientes.Name = "lblSoporteClientes";
+            this.lblSoporteClientes.Size = new System.Drawing.Size(121, 26);
+            this.lblSoporteClientes.TabIndex = 3;
+            this.lblSoporteClientes.Text = "SOPORTE";
+            // 
+            // panaleta2
+            // 
+            this.panaleta2.BackColor = System.Drawing.SystemColors.ControlText;
+            this.panaleta2.Controls.Add(this.panel1);
+            this.panaleta2.Controls.Add(this.lblxSoporteClientes);
+            this.panaleta2.Controls.Add(this.lblSoporteClientes);
+            this.panaleta2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panaleta2.Location = new System.Drawing.Point(0, 0);
+            this.panaleta2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panaleta2.Name = "panaleta2";
+            this.panaleta2.Size = new System.Drawing.Size(800, 28);
+            this.panaleta2.TabIndex = 6;
+            this.panaleta2.Paint += new System.Windows.Forms.PaintEventHandler(this.panaleta_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel1.Location = new System.Drawing.Point(0, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(5, 26);
+            this.panel1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(43, 91);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(542, 332);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(69, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(521, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Detalle los probremas o peticiones que desea solucionar";
+            // 
+            // btnEnviarReporte
+            // 
+            this.btnEnviarReporte.BackColor = System.Drawing.SystemColors.ControlText;
+            this.btnEnviarReporte.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnEnviarReporte.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviarReporte.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnEnviarReporte.Location = new System.Drawing.Point(32, 169);
+            this.btnEnviarReporte.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnEnviarReporte.Name = "btnEnviarReporte";
+            this.btnEnviarReporte.Size = new System.Drawing.Size(118, 142);
+            this.btnEnviarReporte.TabIndex = 9;
+            this.btnEnviarReporte.Text = "Enviar Reporte";
+            this.btnEnviarReporte.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.btnEnviarReporte);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(621, 28);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(179, 422);
+            this.panel2.TabIndex = 10;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel3.Location = new System.Drawing.Point(0, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(5, 26);
+            this.panel3.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.pictureBox1.Image = global::ConsecionarioTecs.Properties.Resources.Captura_de_pantalla_2025_02_18_031942_removebg_preview;
+            this.pictureBox1.Location = new System.Drawing.Point(8, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(169, 88);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // SoporteClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panaleta2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SoporteClientes";
+            this.Text = "SoporteClientes";
+            this.panaleta2.ResumeLayout(false);
+            this.panaleta2.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblxSoporteClientes;
+        private System.Windows.Forms.Label lblSoporteClientes;
+        private System.Windows.Forms.Panel panaleta2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEnviarReporte;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}

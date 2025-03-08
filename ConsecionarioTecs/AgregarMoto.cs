dfde9f9d75382e7a1ce265d47865f6002b6aa5f4
ESCUDERO PLAZA MARIA DEL ROSARIO <mescuderop@msuteq.edu.ec>
2025-03-07 21:30:52 -0500

    rutas

--- ConsecionarioTecs/AgregarMoto.cs
+++ ConsecionarioTecs/AgregarMoto.cs
@@ -15,7 +15,7 @@ namespace ConsecionarioTecs
 {
     public partial class AgregarMoto : Form
     {
-        string connectionString = "Server=DESKTOP-9SMDLH8\\SQLEXPRESS;Database=CompañiaTecsBDD;User id=AleAdmin;Password=161223;";
+        string connectionString = "Server=DESKTOP-9SMDLH8\\SQLEXPRESS;Database=CompañiaTecsBDD;User id=AccesoChari;Password=accesochari;";
         byte[] imagenBytes = null; // Para almacenar la imagen
         public AgregarMoto()
         {
